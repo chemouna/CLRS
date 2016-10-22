60edd634835e4463ba44a5731a32a47683811cc1
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-10-22 14:03:09 +0200

    Start quickcheck prop.

--- clrs-haskell/src/ch01.hs
+++ clrs-haskell/src/ch01.hs
@@ -7,3 +7,7 @@ import Data.Foldable
 insertionSort :: Ord a => [a] -> [a]
 insertionSort = foldr insert []
 
+-- quick check props :
+-- sorting should not change a list length
+sortPreservesLength :: Ord a => [a] -> Bool
+sortPreservesLength xs = length xs == length (insertionSort xs)
