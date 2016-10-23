68120d639db9d9563073108217c754029859891c
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-10-23 03:56:53 +0200

    Exercice 2.1.2

---
+++ clrs-haskell/src/ch02.hs
@@ -0,0 +1,38 @@
+module Ch01 where
+
+import Data.List
+import Data.Foldable
+import Test.QuickCheck
+import Test.QuickCheck.All
+
+insertionSort :: Ord a => [a] -> [a]
+insertionSort = foldr insert []
+
+-- quick check props :
+
+-- sorting should not change a list length
+prop_sortPreservesLength xs = length xs == length (insertionSort xs)
+
+-- sort is idempotent
+prop_sortIdempotent xs = insertionSort (insertionSort xs) == insertionSort xs
+-- to run 500 tests ->  quickCheckWith stdArgs { maxSuccess = 500 } prop_sortIdempotent
+
+prop_sort_nn_min xs = not (null xs) ==> head (insertionSort xs) == minimum xs
+prop_sort_nn_max xs = not (null xs) ==> last (insertionSort xs) == maximum xs
+
+-- Execice 2.1.2
+reverseInsertionSort :: Ord a => [a] -> [a]
+reverseInsertionSort = foldr reverseInsert []
+
+reverseInsert :: Ord a => a -> [a] -> [a]
+reverseInsert x xs = insertBy (flip compare) x xs
+
+-- another way to write reverseInsert
+reverseInsert' :: Ord a => a -> [a] -> [a]
+reverseInsert' x [] = [x]
+reverseInsert' x ls = if x >= head ls
+                        then x:ls
+                        else head ls : reverseInsert' x (tail ls)
+
+prop_reverseInsertionSort xs = reverseInsertionSort xs == reverse (insertionSort xs)
+
