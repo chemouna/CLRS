4f0fb0399b5caadbb0781b50a5d35f35baf08eb2
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-10-23 21:04:43 +0200

    ex 2.2-2

--- clrs-haskell/src/ch02.hs
+++ clrs-haskell/src/ch02.hs
@@ -38,12 +38,33 @@ reverseInsert' x ls = if x >= head ls
 prop_reverseInsertionSort xs = reverseInsertionSort xs == reverse (insertionSort xs)
 
 -- Exercice 2.1.3: search if an element is in a list and return its index
-search :: Ord a => a -> [a] -> Maybe a
+search :: Eq a => a -> [a] -> Maybe a
 search x ls = find (==x) ls
 
-search' :: Ord a => a -> [a] -> Maybe a
-search' v ls = getFirst . foldMap (\x -> First (if v == x then Just x else Nothing))
+-- search' :: Eq a => a -> [a] -> Maybe a
+-- search' v ls = getFirst . foldMap (\ x -> First (if v == x then Just x else Nothing))
 
+-- simple linear search
+searchL :: Eq a => a -> [a] -> Maybe a
+searchL _ [] = Nothing
+searchL v (x:xs) = if v == x then return x else searchL v xs
 
+-- Exercice 2.2-2
+selectionSort :: Ord a => [a] -> [a]
+selectionSort [] = []
+selectionSort xs = let x = maximum xs in selectionSort (remove x xs) ++ [x]
+  where remove _ [] = []
+        remove a (x:xs)
+          | x == a = xs
+          | otherwise = x : remove a xs
 
+selectionSort2 :: Ord a => [a] -> [a]
+selectionSort2 [] = []
+selectionSort2 xs = let x = minimum xs in [x] ++ selectionSort2 (remove x xs)
+  where remove _ [] = []
+        remove a (x:xs)
+          | x == a = xs
+          | otherwise = x : remove a xs
 
+prop_selectionSort xs = insertionSort xs == selectionSort xs
+prop_selectionSort2 xs = insertionSort xs == selectionSort2 xs
