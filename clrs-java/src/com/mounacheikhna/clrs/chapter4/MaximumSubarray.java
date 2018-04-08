d9909673a133bfdb183c96a8eec8bd5b1c5da9a0
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-04-08 09:05:37 +0100

    Ex 4.1.3 from clrs

--- clrs-java/src/com/mounacheikhna/clrs/chapter4/MaximumSubarray.java
+++ clrs-java/src/com/mounacheikhna/clrs/chapter4/MaximumSubarray.java
@@ -2,7 +2,7 @@ package com.mounacheikhna.clrs.chapter4;
 
 public class MaximumSubarray {
 
-    SubArraySum findMaximumSubarray(int[] arr) {
+    SubArraySum findMaximumSubarrayBruteForce(int[] arr) {
         int max = Integer.MIN_VALUE;
         int left = 0;
         int right = 0;
@@ -11,7 +11,7 @@ public class MaximumSubarray {
             sum = 0;
             for (int j = i; j < arr.length; j++) {
                 sum += arr[j];
-                if(max < sum) {
+                if (max < sum) {
                     max = sum;
                     left = i;
                     right = j;
@@ -22,12 +22,57 @@ public class MaximumSubarray {
         return new SubArraySum(left, right, max);
     }
 
+    SubArraySum findMaximumSubarrayRec(int[] arr, int low, int high) {
+        if (high == low) {
+            return new SubArraySum(low, high, arr[low]);
+        } else {
+            int mid = (low + high) / 2;
+            SubArraySum leftSubSum = findMaximumSubarrayRec(arr, low, mid);
+            SubArraySum rightSubSum = findMaximumSubarrayRec(arr, mid + 1, high);
+            SubArraySum crossingSubArray = findMaximumCrossingSubArray(arr, low, mid, high);
+
+            if (leftSubSum.sum >= rightSubSum.sum && leftSubSum.sum >= crossingSubArray.sum) {
+                return leftSubSum;
+            } else if (rightSubSum.sum >= leftSubSum.sum && rightSubSum.sum >= crossingSubArray.sum) {
+                return rightSubSum;
+            } else {
+                return crossingSubArray;
+            }
+        }
+    }
+
+    private SubArraySum findMaximumCrossingSubArray(int[] arr, int low, int mid, int high) {
+        int leftSum = Integer.MIN_VALUE;
+        int sum = 0;
+        int maxLeft = 0;
+        for (int i = mid; i >= low; i--) {
+            sum += arr[i];
+            if (sum > leftSum) {
+                leftSum = sum;
+                maxLeft = i;
+            }
+        }
+        int rightSum = Integer.MIN_VALUE;
+        sum = 0;
+        int maxRight = 0;
+        for (int i = mid + 1; i <= high; i++) {
+            sum += arr[i];
+            if (sum > rightSum) {
+                rightSum = sum;
+                maxRight = i;
+            }
+        }
+        return new SubArraySum(maxLeft, maxRight, leftSum + rightSum);
+    }
+
+    // TODO: test the boundary case of an array with one element
+
     class SubArraySum {
         int left;
         int right;
         int sum;
 
-        public SubArraySum(int left, int right, int sum) {
+        SubArraySum(int left, int right, int sum) {
             this.left = left;
             this.right = right;
             this.sum = sum;
@@ -45,7 +90,17 @@ public class MaximumSubarray {
 
     public static void main(String[] args) {
         MaximumSubarray maximumSubarray = new MaximumSubarray();
-        System.out.println(maximumSubarray.findMaximumSubarray(new int[]{-2, -3, -1, -4}));
+        int[] allNegativeArr = {-2, -3, -1, -4};
+        System.out.println(maximumSubarray.findMaximumSubarrayBruteForce(allNegativeArr));
+        System.out.println(maximumSubarray.findMaximumSubarrayRec(allNegativeArr, 0, allNegativeArr.length - 1));
+
+        int[] clrsArr = {13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7};
+        System.out.println(maximumSubarray.findMaximumSubarrayBruteForce(clrsArr));
+        System.out.println(maximumSubarray.findMaximumSubarrayRec(clrsArr, 0, clrsArr.length - 1));
+
+        int[] oneElementArr = {13};
+        System.out.println(maximumSubarray.findMaximumSubarrayBruteForce(oneElementArr));
+        System.out.println(maximumSubarray.findMaximumSubarrayRec(oneElementArr, 0, oneElementArr.length - 1));
     }
 
 }
